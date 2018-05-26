5d5b8f79d9e28d910837ad9f77ad44278e02f2f9
CaoDuThai <thai.caodu@gmail.com>
2018-05-26 12:48:55 +0700

    Thêm sprite cho config và thêm chức năng thoát game
    Thêm sprite gear vào cho nút config và cho nút Exit của menu có chức năng thoát ứng dụng nhờ MenuExit script

---
+++ Assets/Script/MenuScene/MenuExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuExit : MonoBehaviour {
+
+
+
+
+	//hàm dùng để exit game khi nhấn nút exit ở mainmenu
+	public void ExitGame()
+	{
+		//nếu đang chạy trên unity editor thì thoát khỏi playmode
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else //nếu đang chạy trên phone thì thoát game
+		Application.Quit ();
+		#endif
+		
+	}
+}
