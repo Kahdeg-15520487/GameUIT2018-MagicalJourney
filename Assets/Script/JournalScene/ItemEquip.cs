a5e864fb7c5b48a95576c0d5d00437af6df5ed3f
CaoDuThai <thai.caodu@gmail.com>
2018-05-26 17:06:31 +0700

    Them sprite va script cho scene journal
    them sprite va script

---
+++ Assets/Script/JournalScene/ItemEquip.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemEquip : MonoBehaviour, IPointerDownHandler {
+
+	public Scrollbar ItemScrollBar;
+
+	public void OnPointerDown(PointerEventData eventdata)
+	{
+		
+	}
+
+
+}
