0da42218d9644c9427b831d8cf60b76d9070d3ea
CaoDuThai <thai.caodu@gmail.com>
2018-05-26 22:49:53 +0700

    gan nhu hoan chinh journal scene

---
+++ Assets/Script/JournalScene/EquipmentEquip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipmentEquip : MonoBehaviour {
+
+	private Sprite image;
+
+	public void EquipmentClick()
+	{
+		if (ItemEquip.EquipmentType != null && ItemEquip.EquipmentImage != null) {
+			if (ItemEquip.EquipmentType == "Consumable") {
+				if (this.gameObject.name == "Consumable1" ||
+				    this.gameObject.name == "Consumable2" ||
+				    this.gameObject.name == "Consumable3") {
+					image = ItemEquip.EquipmentImage;
+					transform.parent.GetComponent<Image> ().sprite = image;
+				}
+			} else {
+				if (this.gameObject.name != "Consumable1" ||
+					this.gameObject.name != "Consumable2" ||
+					this.gameObject.name != "Consumable3") {
+					image = ItemEquip.EquipmentImage;
+					transform.parent.GetComponent<Image> ().sprite = image;
+				}
+			}
+		}
+	}
+}
